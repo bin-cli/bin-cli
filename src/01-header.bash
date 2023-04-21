a4dd85c4830c4c9eb5f9b6fa57ee353546e2ca41
Dave James Miller <d@djm.me>
2023-04-21 20:50:25 +0100

    Re-combine the source files - my IDE doesn't cope very well with them being split up

--- src/01-header.bash
+++
@@ -1,9 +0,0 @@
-#!/bin/env bash
-set -o errexit -o nounset -o pipefail
-
-################################################################################
-# Bin - A simple task runner        Copyright (c) 2023 Dave James Miller       #
-# https://bin-cli.com/              MIT License                                #
-################################################################################
-
-VERSION=1.2.3
--- src/04-version.bash
+++
@@ -1,7 +0,0 @@
-debug "Bin version $VERSION"
-
-if $version; then
-    debug_exit "Would print version number"
-    echo "Bin version $VERSION"
-    exit
-fi
