0e52186a41f3be23529e0cf4fb399909b5534d31
Dave James Miller <d@djm.me>
2024-03-31 08:44:15 +0100

    Move .txt temporary files from temp/root/ to temp/

--- features/step_definitions/process_steps.mjs
+++ features/step_definitions/process_steps.mjs
@@ -1,7 +1,7 @@
-import {Given, Then, When} from '@cucumber/cucumber';
+import {After, Given, Then, When} from '@cucumber/cucumber';
 import {strict as assert} from 'assert';
 import {spawnSync} from 'child_process';
-import {ensureDir, exists, outputFile} from 'fs-extra';
+import {ensureDir, exists, outputFile, remove} from 'fs-extra';
 import * as paths from '../support/paths.mjs';
 import * as coverage from '../support/coverage.mjs';
 
@@ -24,7 +24,7 @@ async function run(command, env = {}) {
     env = {
         HOME: `${paths.root}/home/user`,
         PATH: `${paths.root}/usr/bin:${paths.root}/global/bin`,
-        BIN_DEBUG_LOG: `${paths.root}/debug.txt`,
+        BIN_DEBUG_LOG: `${paths.temp}/debug.txt`,
         BIN_TEST_ROOT: paths.root,
         ...env,
         ...(this.env || {}),
@@ -59,13 +59,13 @@ async function run(command, env = {}) {
         env_string += `${key}='${value}' \\\n`;
     }
 
-    await outputFile(`${paths.root}/command.txt`, `cd ${this.workingDir}\n${env_string}${command}\n`);
+    await outputFile(`${paths.temp}/command.txt`, `cd ${this.workingDir}\n${env_string}${command}\n`);
 
     const result = spawnSync(command, {
         cwd: this.workingDir,
         env,
         shell: true,
-        stdio: ['pipe', 'pipe', 'pipe'],
+        stdio: ['ignore', 'pipe', 'pipe'],
     });
 
     if (result.error) {
@@ -76,10 +76,10 @@ async function run(command, env = {}) {
 
     // Write the output to files to be displayed by the 'bin/tdd' script if the test fails
     const stdout = result.stdout.toString();
-    await outputFile(`${paths.root}/stdout.txt`, stdout);
+    await outputFile(`${paths.temp}/stdout.txt`, stdout);
 
     const stderr = result.stderr.toString();
-    await outputFile(`${paths.root}/stderr.txt`, stderr);
+    await outputFile(`${paths.temp}/stderr.txt`, stderr);
 
     this.runResult = {status, stdout, stderr};
 }
@@ -153,4 +153,16 @@ Then('the error is:', function (expected) {
 Then('the error is {string}', function (expected) {
     expected = paths.replace(expected);
     assert.equal(this.runResult.stderr, `${expected}\n`);
+});
+
+After({name: 'Remove temp files', tags: 'not @exit'}, async function (hook) {
+    // If the test failed, keep the temp files for inspection
+    if (hook.result.status === 'FAILED') {
+        return 'skipped';
+    }
+
+    await remove(`${paths.temp}/command.txt`);
+    await remove(`${paths.temp}/stdout.txt`);
+    await remove(`${paths.temp}/stderr.txt`);
+    await remove(`${paths.temp}/debug.txt`);
 });
