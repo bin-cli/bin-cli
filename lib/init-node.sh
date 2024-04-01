e5c7e1d023aeb54cbcdca938f7f67a7decdd1705
Dave James Miller <d@djm.me>
2024-04-01 13:01:42 +0100

    Automatically install node modules

--- lib/init-node.sh
+++ lib/init-node.sh
@@ -1,5 +1,9 @@
 if [[ -z ${GITHUB_ACTION-} && -d $HOME/.nvm ]]; then
     export NVM_DIR="$HOME/.nvm"
     source "$NVM_DIR/nvm.sh"
-    nvm use >/dev/null
+    nvm use --silent
+fi
+
+if [[ ! -d node_modules ]]; then
+    npm ci --silent
 fi
