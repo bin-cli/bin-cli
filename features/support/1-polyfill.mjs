e3c746c98f95f93a74d722e8a0e5f5c40d69ff9a
Dave James Miller <d@djm.me>
2024-04-01 08:06:47 +0100

    Remove polyfill; Downgrade Gherkin to match the version used by Cucumber 10.3

--- features/support/1-polyfill.mjs
+++
@@ -1,8 +0,0 @@
-import replaceAll from 'string.prototype.replaceall';
-
-// Added in Node.js v15
-if (!('replaceAll' in String.prototype)) {
-    String.prototype.replaceAll = function(...args) {
-        return replaceAll(this, ...args);
-    }
-}
